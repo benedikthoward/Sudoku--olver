c87517766b0d9536cdf0892a9043bd565bfe686d
benedikthoward <benedikthoward@gmail.com>
2023-11-09 01:03:17 -0800

    yes

--- SudokuStuff.h
+++ SudokuStuff.h
@@ -1,62 +1,136 @@
-//sorts the index into what 3x3 box it is in
-int box(int r, int c)
-{
-    if (r<3)
-    {
-        if (c<3)
-        {
-            return 0;
-        }else if (c<6)
-        {
-            return 1;
-        }else
-        {
-            return 2;
-        }
-    }else if (r<6)
-    {
-        if (c<3)
-        {
-            return 3;
-        }else if (c<6)
-        {
-            return 4;
-        }else
-        {
-            return 5;
-        }
-    }else
-    {
-        if (c<3)
-        {
-            return 6;
-        }else if (c<6)
-        {
-            return 7;
-        }else
-        {
-            return 8;
-        }
-    }
+
+void fill_possibilities(unsigned char possibilities[9][9][9], unsigned char filled[9][9]){
+    for(int i = 0; i<9;i++){
+        for(int j = 0;j<9;j++){
+            if(filled[i][j]==0){
+                for(int k = 1;k<10;k++){
+                    if(found_in_scope){
+                        possibilities[i][j][k]=0;
+                    }else{
+                        possibilities[i][j][k]=1;
+                    }
+                }
+            }
+        }    
+    }    
 }
 
-int doesNotContain(int a, int yes[80]){
-    for(int i=0;i<80;i++){
-        if(a==yes[i]){
-            return 0;
+int found_in_scope(int i, int j, int looking_for, unsigned char filled[9][9]){
+    for(int increm = 0; increm<9;increm++){
+        if(filled[increm][j]==looking_for||filled[i][increm]==looking_for){
+            return 1;
+        }
+    }
+    
+    int left = (i<3)? 0:(i<6) ? 3: 6;
+    int top = (j<3)? 0:(j<6) ? 3: 6;
+
+    for(int a = left; a<left+3;a++){
+        for(int b = top; b< top+3; b++){
+            if(filled[a][b]==looking_for){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int fill_conclusive(unsigned char possibilities[9][9][9], unsigned char filled[9][9]){
+    int boolean = 0;
+    int sum;
+    int index;
+    for(int i = 0;i<9;i++){
+        for(int j = 0; j<9;j++){
+            if(filled[i][j]==0){
+                sum = 0;
+                for(int k =0 ;k<9;k++){
+                    sum+=possibilities[i][j][k];
+                    if(possibilities[i][j][k]==1){
+                        index = k+1;
+                    }
+                }
+                if(sum ==1){
+                    filled[i][j] = k;
+                    boolean =1;
+                }
+            }
+        }    
+    }
+    return boolean;
+}
+
+//encodes a decision [index_Y, index_X, number_of_Outcomes, n1, n2, ...]
+int* decision_Array(unsigned char possibilities[9][9][9],unsigned char sud[9][9]) {
+    int min_outcomes = 2,147,483,647;
+    int sum;
+    int x;
+    int y;
+    for(unsigned char i = 0;i<9;i++){
+        for(unsigned char j = 0; j<9;j++){
+            sum = 0;
+            if(sud[i][j]==0){
+                for(unsigned char k =0 ;k<9;k++){
+                    sum += possibilities[i][j][k];
+                }
+                if(sum<min_outcomes){
+                    x = j;
+                    y = i;
+                    min_outcomes = sum;
+                }
+            }
+        }
+    }
+    // Dynamically allocate an array of 'size' integers
+    int* array = (int*)malloc((3+min_outcomes) * sizeof(int));
+
+    array[0] = y
+    array[1] = x;
+    array[2] = min_outcomes;
+    int preveous = 0;
+    for (int i = 3; i < size; ++i) {
+        for(int j = preveous;j<9;i++){
+            if(possibilities[y][x][j]==1){
+                array[i] = j+1;
+                preveous = j+1;
+                break;
+            }
+        }
+    }
+    
+    // Return the pointer to the array
+    return array;
+}
+
+int is_Valid(unsigned char possibilities[9][9][9],unsigned char sud[9][9]){
+    for(unsigned char i = 0;i<9;i++){
+        for(unsigned char j = 0; j<9;j++){
+            sum = 0;
+            if(sud[i][j]==0){
+                for(unsigned char k =0 ;k<9;k++){
+                    sum += possibilities[i][j][k];
+                }
+                if(sum==0){
+                    return 0;
+                }
+            }
         }
     }
     return 1;
 }
-void IndexToVector(int index, int *Xcoordinate, int *Ycoordinate)
-{
-    int x = index%9;
-    int y = (index)/9;
-    *Xcoordinate = x;
-    *Ycoordinate = y;
-}
 
-struct nonRes{
-    int numberOfRestrictions;
-    int index[80];
-};
+int (*decision_copy_array(int x, int y, int val, int sud[9][9]))[9] {
+    // Allocate memory for a 9x9 array of integers
+    int (*copy)[9] = malloc(9 * sizeof(*copy));
+    
+
+    // Copy the original array and change the specified value
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            copy[i][j] = sud[i][j];
+        }
+    }
+
+    copy[x][y] = val;
+    
+    return copy;
+}
--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "SudokuStuff.h"
 
+typedef struct Generational_state{
+    unsigned char generational_field[9][9];
+    unsigned char possibilities[9][9][9];
+}Generational_state;
 
+typedef struct Generation{
+    Generational_state *offspring;
+}Generation;
 
 int main() {
-    int sud[9][9] = {{0, 3, 0, 9, 2, 0, 4, 0, 1},
+    unsigned char sud[9][9] = {{0, 3, 0, 9, 2, 0, 4, 0, 1},
                      {0, 1, 0, 3, 0, 4, 0, 0, 0},
                      {4, 0, 7, 6, 0, 0, 0, 9, 3},
                      {0, 0, 0, 0, 0, 0, 0, 6, 5},
@@ -13,192 +20,18 @@ int main() {
                      {7, 5, 0, 4, 6, 0, 0, 3, 0},
                      {3, 0, 6, 0, 5, 1, 0, 4, 0},
                      {0, 4, 0, 0, 7, 3, 0, 0, 0}};
-//get the user to input the sudoku problem
 
-
-
-    //prints the sudoku problem
-    int unknowns = 0;
-    for (int r = 0; r < 9; r++) {
-        for (int c = 0; c < 9; c++) {
-            printf("%d", sud[r][c]);
-            if (sud[r][c] == 0) {
-                unknowns++;
-            }
-            if (c < 8) {
-                printf("%s", "|");
-            }
-        }
-        printf("\n");
-    }
-    printf("\n");
-    printf("\n");
-    int solMatrix[28][82];
-//fill the matrix with zeros
-    for (int r = 0; r < 28; r++) {
-        for (int c = 0; c < 82; c++) {
-            solMatrix[r][c] = 0;
-        }
-
+    unsigned char possibilities [9][9][9];
+    //fill_conclusive returns if it made a change based on a field that is actually known 
+    while(fill_conclusive(possibilities,sud)) {
+        fill_possibilities(possibilities,sud);
     }
 
-// fill the "known column" with the maximum sum of a row, column, box
-    solMatrix[0][unknowns] = -1;
-    for (int i = 1; i < 28; i++) {
-        solMatrix[i][unknowns] = 45;
+    int decision[] =  decision_Array(possibilities,sud);
+    Generation gen1;
+    gen1.offspring = (Generational_state *)malloc(decision[2] * sizeof(Generational_state));
+    for(int i = 0;i<decision_Array(possibilities,sud)[2]; i++){
+        gen1.offspring[i].generational_field = decision_copy_array(decision[1],decision[0],decision[i+3],sud);
     }
-
-//fills the equation matrix with ones for the unknowns and o for knowns. knowns also get subtractd from the total for the equation
-    int unknownsPlaced = 0;
-    int knownsPlaced = 0;
-    for (int r = 0; r < 9; r++) {
-        for (int c = 0; c < 9; c++) {
-
-            if (sud[r][c] == 0) {
-                solMatrix[0][unknownsPlaced] = (r * 9) + c;
-                //places the value into the row equation
-                solMatrix[r + 1][unknownsPlaced] = 1;
-                //places the value into the column equation
-                solMatrix[c + 10][unknownsPlaced] = 1;
-                //places the value into the bo equation
-                solMatrix[19 + box(r, c)][unknownsPlaced] = 1;
-                unknownsPlaced++;
-
-            } else {
-                solMatrix[0][unknowns + knownsPlaced + 1] = (r * 9) + c;
-                //places the value into the row equation
-                solMatrix[r + 1][unknowns + knownsPlaced + 1] = sud[r][c];
-                solMatrix[r + 1][unknowns] -= sud[r][c];
-                //places the value into the column equation
-                solMatrix[c + 10][unknowns + knownsPlaced + 1] = sud[r][c];
-                solMatrix[c + 10][unknowns] -= sud[r][c];
-                //places the value into the bo equation
-                solMatrix[19 + box(r, c)][unknowns + knownsPlaced + 1] = sud[r][c];
-                solMatrix[19 + box(r, c)][unknowns] -= sud[r][c];
-                knownsPlaced++;
-            }
-        }
-    }
-    //prints the sudoku problem Matrix
-    for (int r = 0; r < 28; r++) {
-        for (int c = 0; c < 82; c++) {
-            printf("%d", solMatrix[r][c]);
-            if (c < 81) {
-                printf("%s", "|");
-            }
-        }
-        printf("\n");
-    }
-
-    int indexOfGuesses[80];
-    int numberOfGuesses = 0;
-    int acceptableValue;
-    for (int l = 0; l < 80; l++) {
-        indexOfGuesses[l] = -1;
-    }
-
-//finds a combination on columns that keeps all 27 equations in tact and gets down to 27 unknowns. that matrix will be re-arranged
-    for (int i = 0; i < unknowns; i++) {
-        acceptableValue = 1;
-        for (int j = 1; j < 28; j++) {
-            int rowTotal = 0;
-            for (int k = 0; k < unknowns; k++) {
-                if (doesNotContain(solMatrix[0][k], indexOfGuesses) == 1 && k != i) {
-                    rowTotal += solMatrix[j][k];
-
-                }
-            }
-            if (rowTotal == 0) {
-                acceptableValue = 0;
-                break;
-
-            }
-        }
-        if (acceptableValue == 1) {
-            indexOfGuesses[numberOfGuesses] = solMatrix[0][i];
-            numberOfGuesses++;
-        }
-        if (numberOfGuesses == unknowns - 27) {
-            break;
-        }
-    }
-
-    printf("\n\n");//prints the columns indices that satisfy the directions given above.
-    for (int l = 0; l < 80; l++) {
-        printf("%d,", indexOfGuesses[l]);
-    }
-
-
-    int solM2[28][82];
-    int solM2Filled = 0;
-
-    printf("\n\n");
-    
-    for(int i=0;i<unknowns;i++){
-        if(doesNotContain(solMatrix[0][i],indexOfGuesses)==1){
-            for(int r=0;r<28;r++){
-                int a = solMatrix[r][i];
-                solM2[r][solM2Filled]=a;
-            }
-            solM2Filled++;
-        }
-    }
-    
-
-    for(int i=0;i<28;i++){
-        solM2[i][27]=solMatrix[i][unknowns];
-    }
-
-    
-    for(int i=0;i<unknowns;i++){
-        if(doesNotContain(solMatrix[0][i],indexOfGuesses)==0){
-            for(int r=0;r<28;r++){
-                if(solMatrix[r][i]>0&&r>0){
-                    solM2[r][solM2Filled+1]=-1;
-                }else{
-                    solM2[r][solM2Filled+1]=solMatrix[r][i];
-                }
-            }
-            solM2Filled++;
-        }
-    }
-    for(int i=solM2Filled+1;i<82;i++){
-        for(int r=0;r<28;r++){
-                solM2[r][solM2Filled]=solMatrix[r][i];
-            
-        }
-        solM2Filled++;
-     }
-    
-
-    
-    printf("\n\n");
-    for (int r = 0; r < 28; r++) {
-        for (int c = 0; c < 82; c++) {
-            printf("%d", solM2[r][c]);
-            if (c < 82) {
-                printf("%s", "|");
-            }
-        }
-        printf("\n");
-    }
-    int reducable[27][28];
-    int knownandGuess[27][54];
-
-    for(int r=0;r<27;r++){
-        for(int c = 0; c<28;c++){
-            reducable[r][c]=solM2[r+1][c];
-            printf("%d|", reducable[r][c]);
-        }
-        printf("\n");
-    }
-
-    for(int r=0;r<27;r++){
-        for(int c = 28; c<82;c++){
-            knownandGuess[r][c-28]=solM2[r+1][c];
-            printf("%d|", knownandGuess[r][c-28]);
-        }
-        printf("\n");
-    }
+    return 0;
 }
-
